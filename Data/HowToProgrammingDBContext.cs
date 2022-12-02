104c143219ad7a258e01598dbc359eb3204c02d0
Ertan Taner <ertantaner2000@gmail.com>
2022-12-03 00:10:04 +0300

    feat: languageparadigm table added to context

--- Data/HowToProgrammingDBContext.cs
+++ Data/HowToProgrammingDBContext.cs
@@ -22,7 +22,7 @@ namespace HowToProgramming.Data
         DbSet<LanguageLevel> LanguageLevel {get;set;}
         DbSet<Paradigm> Paradigms {get;set;}
         DbSet<Typee> Typees {get;set;}
-        
+        DbSet<LanguageParadigm> LanguageParadigms {get;set;}        
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
             new CreatorConfig().Configure(modelBuilder.Entity<Creator>());
             new LanguageConfig().Configure(modelBuilder.Entity<Language>());
