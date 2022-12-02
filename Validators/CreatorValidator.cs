00a7ba641ac5e9a4c2cd9c5f70c2a057ecdb1579
Ertan Taner <ertantaner2000@gmail.com>
2022-12-02 22:28:52 +0300

    fixed: tree and revert problem solved

--- Validators/CreatorValidator.cs
+++ Validators/CreatorValidator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentValidation;
+using HowToProgramming.Helpers;
 using HowToProgramming.Models;
 
 namespace HowToProgramming.Validators
@@ -10,7 +11,27 @@ namespace HowToProgramming.Validators
     public class CreatorValidator : AbstractValidator<Creator>
     {
         public CreatorValidator() {
-            RuleFor()
+            RuleFor(c => c.Name)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty().WithMessage("{PropertyName} is empty, please write your name");
+            
+
+            RuleFor(c => c.GenderId)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty().WithMessage("{PropertyName} is empty, please choose your gender")
+            .LessThan(99).GreaterThanOrEqualTo(1).WithMessage("Please select a valid {PropertyName}");
+
+            RuleFor(c => c.LanguageId)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty().WithMessage("{PropertyName} is empty, please choose your language")
+            .LessThan(99).GreaterThanOrEqualTo(1).WithMessage("Please select a valid {PropertyName}");
+
+
+            RuleFor(c => c.FrameworkId)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty().WithMessage("{PropertyName} is empty, please choose a framework")
+            .LessThan(200).GreaterThanOrEqualTo(1).WithMessage("Please select valid {PropertyNaame}");
         }
+
     }
 }
