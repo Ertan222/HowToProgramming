6e7f396f1678e0bb6d82ec46dbcd2afc533ba615
Ertan Taner <ertantaner2000@gmail.com>
2022-12-04 02:17:30 +0300

    test: Test done for date fluent

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,15 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using HowToProgramming.Models;
+using HowToProgramming.Data;
+using FluentValidation;
+using FluentValidation.Results;
 
 namespace HowToProgramming.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
